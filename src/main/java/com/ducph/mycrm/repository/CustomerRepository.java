553d7f3b3e867699c984d05ba35b776aabcaa9e7
hongducphan <hongducphan912@gmail.com>
2020-06-08 21:21:36 +0700

    Update Spring Data REST & Google OAuth2 security

--- src/main/java/com/ducph/mycrm/repository/CustomerRepository.java
+++ src/main/java/com/ducph/mycrm/repository/CustomerRepository.java
@@ -2,9 +2,17 @@ package com.ducph.mycrm.repository;
 
 import com.ducph.mycrm.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "customers", itemResourceRel = "customer")
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    List<Customer> findByFirstNameOrLastNameOrEmailLike(String firstName, String lastName, String email);
+
+    @RestResource(path = "/withValue")
+    @Query("from Customer where firstName like %:value% or lastName like %:value% or email like %:value%")
+    List<Customer> search(@Param("value") String value);
 }
