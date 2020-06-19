13aa25b23d51be59576e975d0f7d640328448812
Phan Hong Duc <hongducphan912@gmail.com>
2020-06-19 18:45:03 +0700

    - Update exception handling - Update search function

---
+++ src/main/java/com/ducph/mycrm/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.ducph.mycrm.controller;
+
+import com.ducph.mycrm.entity.Customer;
+import com.ducph.mycrm.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    
+    @Autowired
+    private CustomerService customerService;
+    
+    @GetMapping("/search/{value}")
+    public List<Customer> search(@PathVariable("value") String value) {
+        return customerService.search(value);
+    }
+}
--- src/main/java/com/ducph/mycrm/repository/CustomerRepository.java
+++ src/main/java/com/ducph/mycrm/repository/CustomerRepository.java
@@ -5,14 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "customers", itemResourceRel = "customer")
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-    @RestResource(path = "/withValue")
     @Query("from Customer where firstName like %:value% or lastName like %:value% or email like %:value%")
     List<Customer> search(@Param("value") String value);
 }
