2627f968a00c5d7c3a7e5829a3ac882569bdea72
Phan Hong Duc <hongducphan912@gmail.com>
2020-07-05 00:49:19 +0700

    Using custom response object format without HATEOAS

--- src/main/java/com/ducph/mycrm/service/CustomerService.java
+++ src/main/java/com/ducph/mycrm/service/CustomerService.java
@@ -5,10 +5,15 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface CustomerService {
 
+    Map<String, Object> findAll(Pageable pageable);
+
+    Optional<Customer> findById(int id);
+
     List<Customer> criteriaSearch(String value);
-    
+
     Map<String, Object> searchByCustomer(String firstName, String lastName, String email, Pageable pageable);
 }
--- src/main/java/com/ducph/mycrm/util/ApplicationUtils.java
+++ src/main/java/com/ducph/mycrm/util/ApplicationUtils.java
@@ -1,7 +1,11 @@
 package com.ducph.mycrm.util;
 
+import org.springframework.data.domain.Page;
+
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public interface ApplicationUtils {
 
@@ -12,5 +16,15 @@ public interface ApplicationUtils {
 
     static String getCurrentDateTime() {
         return String.valueOf(new Date(new Timestamp(System.currentTimeMillis()).getTime()));
+    }
+
+    static Map<String, Object> convertToPagingFormat(Page<?> data) {
+        var result = new LinkedHashMap<String, Object>();
+
+        result.put("content", data.getContent());
+        result.put("totalPages", data.getTotalPages());
+        result.put("totalElements", data.getTotalElements());
+
+        return result;
     }
 }
